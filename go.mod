5adc09df497cbbcd22ccc76e4c7e4dcfb5e48d4c
salledelavage <salledelavager@gmail.com>
2024-03-10 14:06:08 -0400

    change go module

--- go.mod
+++ go.mod
@@ -10,11 +10,16 @@ require (
 	github.com/bitly/go-simplejson v0.5.1 // indirect
 	github.com/bmizerany/assert v0.0.0-20160611221934-b7ed37b82869 // indirect
 	github.com/elazarl/goproxy v0.0.0-20231117061959-7cc037d33fb5 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
 	github.com/moul/http2curl v1.0.0 // indirect
 	github.com/parnurzeal/gorequest v0.3.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/smartystreets/goconvey v1.8.1 // indirect
+	github.com/zmb3/spotify/v2 v2.4.1 // indirect
 	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/oauth2 v0.0.0-20210810183815-faf39c7919d5 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,31 +1,51 @@
 package main
 
 import (
-	"fmt"
-  "github.com/rapito/go-spotify/spotify"
+	
 	"github.com/joho/godotenv"
-	"os"
+	"github.com/zmb3/spotify/v2"
 	"log"
+	"os"
 )
 
-
 func main() {
-
-	err := godotenv.Load(".env")
-  if err != nil {
-    log.Fatal("Error loading .env file")
-  }
-  clientid := os.getenv("CLIENT_ID")
-  clientsecret := os.getenv("CLIENT_SECRET")
-
-  fmt.Println("vachier")	
 	call_api()
 }
 
 func call_api() {
-    
-  spot := spotify.New(clientid,clientsecret)
-  result, _ := spot.Get("albums/%s", nil, "0sNOF9WDwhWunNAHPD3Baj")
-  
-  fmt.Println(string(result))	
+
+	// the redirect URL must be an exact match of a URL you've registered for your application
+	// scopes determine which permissions the user is prompted to authorize
+	auth := spotifyauth.New(spotifyauth.WithRedirectURL(redirectURL), spotifyauth.WithScopes(spotifyauth.ScopeUserReadPrivate))
+
+	// get the user to this URL - how you do that is up to you
+	// you should specify a unique state string to identify the session
+	url := auth.AuthURL(state)
+
+}
+
+func get_creds() (string, string) {
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	clientid := os.Getenv("SPOTIFY_ID")
+	clientsecret := os.Getenv("SPOTIFY_SECRET")
+	return clientid, clientsecret
+}
+
+// the user will eventually be redirected back to your redirect URL
+// typically you'll have a handler set up like the following:
+func redirectHandler(w http.ResponseWriter, r *http.Request) {
+	// use the same state string here that you used to generate the URL
+	token, err := auth.Token(r.Context(), state, r)
+	if err != nil {
+		http.Error(w, "Couldn't get token", http.StatusNotFound)
+		return
+	}
+	// create a client using the specified token
+	client := spotify.New(auth.Client(r.Context(), token))
+
+	// the client can now be used to make authenticated requests
 }
